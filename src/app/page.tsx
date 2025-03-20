9f29152de9ef3fe29a9eb9ac94071ba548cec90e
paras <sharmaparas388@gmail.com>
2025-03-20 21:34:16 +0530

    add suspense

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 import Main from "@/components/Home/Main";
+import { Suspense } from "react";
 
 export default function Home() {
   return (
     <div>
+    <Suspense fallback={<div>Loading...</div>}>
       <Main />
+    </Suspense>
     </div>
   );
 }
